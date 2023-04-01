acc41858b89e0c4fd85513c64608240a471c1438
rizwana337 <120568097+rizwana337@users.noreply.github.com>
2023-04-01 15:07:28 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,41 @@
 import React from 'react'
 import '../styles/App.css';
+
 const App = () => {
 
   const handleDoubleClick = (event) =>{
-  
+    if (event.detail === 2) {
+      console.log("I was double clicked");
+    } else {
+      console.log("I was not double clicked");
+    }
   }
+  
   return (
     <div id="main">
-      <button id="dblclick-btn">Double click me</button>
+      <button id="dblclick-btn" onDoubleClick={handleDoubleClick}>Double click me</button>
     </div>
   )
 }
 
-
 export default App;
+
+
+
+
+// import React from 'react'
+// import '../styles/App.css';
+// const App = () => {
+
+//   const handleDoubleClick = (event) =>{
+  
+//   }
+//   return (
+//     <div id="main">
+//       <button id="dblclick-btn">Double click me</button>
+//     </div>
+//   )
+// }
+
+
+// export default App;
